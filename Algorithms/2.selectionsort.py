49147d7adbd40409032a43f0ccbe67b39cf1b7e4
Hema <118927072+Kemuhulk@users.noreply.github.com>
2025-02-27 20:40:33 +0530

    Create 2.selectionsort.py

---
+++ Algorithms/2.selectionsort.py
@@ -0,0 +1,12 @@
+def selectionsort(arr):
+  n = len(arr)
+  for i in range(n-1):
+    min_index = i
+    for j in range(i+1, n): # find the min element
+      if arr[j] < arr[min_index]:
+        min_index = j
+    arr[i], arr[min_index] = arr[min_index], min[i]
+    
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+print("Sorted array:", arr)
